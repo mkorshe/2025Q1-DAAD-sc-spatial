335312900337cf276c51cff2ce69562a838c0e4c
mkorshe <korshe.mar@gmail.com>
2025-02-26 11:46:26 +0100

    updated

--- Module_2/S03/DAAD_M13_scrnaseq.R
+++ Module_2/S03/DAAD_M13_scrnaseq.R
@@ -1,6 +1,5 @@
 # Package installation
-
-remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
+#remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
 
 
 remotes::install_github("satijalab/seurat-data", "seurat5", quiet = TRUE)
